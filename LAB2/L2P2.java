23825e723a786dba4485ff3a29503cbbe76079f4
janvitha2006 <janvithap108@gmail.com>
2026-02-17 15:00:17 +0530

    Add files via upload

---
+++ LAB2/L2P2.java
@@ -0,0 +1,32 @@
+import java.lang.*;
+import java.util.Scanner;
+class L2P2{
+	public static void main(String...args){
+	System.out.println("enter the size of numbers");
+	Scanner s=new Scanner(System.in);
+	int n=s.nextInt;
+	
+	int[] a=new int[n];
+	
+	for(i=0 ; i<a.length; i++){
+		a[i] = s.nextInt();
+		}
+	for(int v:a){
+		int count=1;
+		}
+	for(int j=0;j<=a;j++){
+	if(a%j==0){
+		count++;
+	}
+	if(count==20)
+	c++;
+   	}
+	System.out.println("c");
+	}
+} 
+
+			
+		
+	
+	
+ 
