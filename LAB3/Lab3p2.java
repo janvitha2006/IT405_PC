e3765a35c69d13afd7b99bfab5ee97824d9eea45
janvitha2006 <janvithap108@gmail.com>
2026-02-17 15:12:18 +0530

    Add files via upload

---
+++ LAB3/Lab3p2.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+import java.util.Scanner;
+class Lab3P2 extends Lab3P1{
+	static int neu;
+
+	public static void display(){
+		System.out.println(view+nue);
+	}
+
+	public int show(){
+		return(view+nue);
+	}
+
+	public void peal(){
+		System.out.println("I am child of parent");
+	}
+}
+
+		
+	
