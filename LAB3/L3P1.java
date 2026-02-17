94d2ef5b78b35f22d7882bb182ca2fd7454d3197
janvitha2006 <janvithap108@gmail.com>
2026-02-17 15:10:50 +0530

    Update and rename Lab3 to LAB3/L3P1.java

---
+++ LAB3/L3P1.java
@@ -0,0 +1,14 @@
+
+import java.lang.*;
+import java.util.Scanner;
+class Lab3P1{
+	static int view;
+	
+	public static void display(){
+		System.out.println(view);
+	}
+	
+	public static int show(){
+		return(view);
+	}
+}
