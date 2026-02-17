0a5d540d11a35ec3f912c20aa2eef223557b8484
janvitha2006 <janvithap108@gmail.com>
2026-02-17 15:32:32 +0530

    Create L4P1.java

---
+++ LAB4/L4P1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class AgeException extends Exception{
+	public AgeException(String message){
+		super(message);
+	}
+}
+class ExceptionMain{
+	public static void main(String...args){
+		Scanner s=new Scanner(System.in);
+		
+		try{
+			int age=s.nextInt();
+			if(age<18)
+				throw new AgeException("Sorry you are not eligible");
+			else
+				System.out.println("Please cast your vote");
+		}catch(AgeException ae){
+			System.out.println(ae.getMessage());
+		}
+	}
+}
