68d6ce4bbd75a9378a7fd2591127c7f77e897a90
janvitha2006 <janvithap108@gmail.com>
2026-02-17 16:29:53 +0530

    Create L4P3.java

---
+++ LAB4/L4P3.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class FileMain{
+	public static void main(String...args)throws IOException{
+		File f=new File("Epson.java");
+		if(f.exists()){
+			System.out.println("Yes this file exists");
+			}
+		else{
+			f.createNewFile();
+			System.out.println("File Exists"+f.exists());
+		}
+	}
+}
